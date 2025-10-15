00da4aeb6725c65da321e0f9e5274187e3da9da0
Pieter Witvoet <p.witvoet@gmail.com>
2025-10-15 23:50:38 +0200

    Fixed that the alpha channel was being ignored for 32-bit tga files whose image descriptor byte incorrectly indicated that pixels had 0 bits of alpha data.
    Tga files created with Developer's Image Library (which is used by VTFEdit, among others) specifically seem to have this byte set incorrectly, though there may be other libraries that do this as well.

--- Shared/FileFormats/ImageReader.cs
+++ Shared/FileFormats/ImageReader.cs
@@ -1,4 +1,5 @@
-﻿using SixLabors.ImageSharp;
+﻿using Shared.FileFormats.Indexed;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace Shared.FileFormats
@@ -15,7 +16,50 @@ namespace Shared.FileFormats
 
         public Image<Rgba32> ReadImage(string path)
         {
+            if (Path.GetExtension(path).ToLowerInvariant() == ".tga")
+                return LoadTga(path);
+
             return Image.Load<Rgba32>(path);
+        }
+
+
+        /// <summary>
+        /// Some libraries create 32-bit RGBA tga files where the image descriptor byte doesn't correctly indicate that each pixel has 8 bits of extra information (alpha channel),
+        /// causing ImageSharp to ignore the alpha channel data entirely. This method patches that byte to ensure that such tga files are loaded correctly.
+        /// </summary>
+        private Image<Rgba32> LoadTga(string path)
+        {
+            using (var file = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                if (!HasIncorrectTgaImageDescriptor(file))
+                    return Image.Load<Rgba32>(file);
+
+
+                file.Position = 17;
+                var imageDescriptorByte = file.ReadByte();
+                file.Position = 0;
+                using (var stream = new PatchingStream(file, true))
+                {
+                    // Patch the image descriptor byte to properly report 8 bits of extra data per pixel:
+                    stream.AddPatch(17, new byte[] { (byte)(imageDescriptorByte | 0x08) });
+                    return Image.Load<Rgba32>(stream);
+                }
+            }
+        }
+
+        private bool HasIncorrectTgaImageDescriptor(Stream stream)
+        {
+            var pos = stream.Position;
+
+            var header = stream.ReadBytes(18);
+            var isUncompressedRGB = header[2] == 2;
+            var hasNoColorMap = header[5] == 0 && header[6] == 0;
+            var is32Bpp = header[16] == 32;
+            var hasZeroAlphaBits = header[17] == 0;
+
+            stream.Position = pos;
+
+            return isUncompressedRGB && is32Bpp && hasNoColorMap && hasZeroAlphaBits;
         }
     }
 }
